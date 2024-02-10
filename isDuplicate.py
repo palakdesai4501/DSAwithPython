ebd949a735ec2c296f6ac2c2523c69e160c0ec7b
Palak <palak.desai4501@gmail.com>
2024-02-10 18:36:38 -0500

    Feature: Created hashset to insert value and check duplicates

--- isDuplicate.py
+++ isDuplicate.py
@@ -1,3 +1,14 @@
 #Given an array of integers, find if the array contains any duplicates. 
 #Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct. 
 nums = [1,2,3,1]
+
+def isDuplicate(nums):
+    hashset = set()
+
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+
+print(isDuplicate(nums))
