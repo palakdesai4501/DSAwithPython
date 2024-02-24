4686fb0216a418157b5fbbdcb8a3371d8f8e142c
Palak <palak.desai4501@gmail.com>
2024-02-23 22:00:22 -0500

    Feature: Compared two strings and gave the result

--- validAnagram.py
+++ validAnagram.py
@@ -1,7 +1,15 @@
 #Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-def anagram(str1,str2):
-    return sorted(str1) == sorted(str2)
+def isAnagram(self, s, t):
+    str1_lo = s.lower()
+    str2_lo = t.lower()
 
+    return set(str1_lo) == set(str2_lo)
 
+string1 = "hello"
+string2 = "olleh"
+
+result = isAnagram(string1, string2)
+
+print(result)
 
 
